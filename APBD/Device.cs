8ac4420f0f075d873985caa78a748e0752628344
s30151 <s30151.pjwstk.edu.pl>
2025-03-18 11:52:23 +0100

    added Device abstract class

---
+++ APBD/Device.cs
@@ -0,0 +1,17 @@
+﻿public abstract class Device
+{
+   public int Id { get; set; }
+   public string Name { get; set; }
+   public bool IsDeviceTurnedOn { get; set; }
+
+   public void TurnOn()
+   {
+      IsDeviceTurnedOn = true;
+      Console.WriteLine("Device is on");
+   }
+   public void TurnOff()
+   {
+      IsDeviceTurnedOn = false;
+      Console.WriteLine("Device is off");
+   }
+}
